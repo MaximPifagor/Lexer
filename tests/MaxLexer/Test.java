ba1071dd873333f332e6fa5db7347986786d476f
pifagor <maksimgalimzanov808@gmail.com>
2019-12-28 17:12:00 +0500

    Refactoring

--- tests/MaxLexer/Test.java
+++ tests/MaxLexer/Test.java
@@ -13,11 +13,16 @@ public class Test {
         lexer.register(new SingleLineCommentReader());
         lexer.register(new NumberReader());
 
-        Token[] tokens = lexer.tokenize("56 /* fdgfdgdf\n dfsdfd */\n if(1>0) print('100') //Foo\n dfsfsdg //\n");
+        Token[] tokens = lexer.tokenize("56 /* fdgfdgdf\n" +
+                " dfsdfd */\n" +
+                " if(1>0) print('100') //Foo\n" +
+                " dfsfsdg //\n");
 
-        for (int i = 0; i < tokens.length; i++) {
+        for (int i = 0; i < tokens.length; i++)
+        {
             System.out.println(tokens[i].value.replace(' ', '_'));
         }
+
         System.out.println("-------------------");
     }
 }
